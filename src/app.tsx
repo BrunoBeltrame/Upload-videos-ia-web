06b432f6dd2dbcdd633729d60998cf6057e3a951
Bruno Beltrame <brunobeltrame2004@gmail.com>
2026-06-09 20:15:26 -0300

    Alteração em div do app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -39,7 +39,7 @@ export function App() {
 
         <div className="flex items-center gap-3">
           <span className="text-sm text-muted-foreground">
-            Desenvolvido com 💜 no NLW da Rocketseat
+            Desenvolvido por Bruno Beltrame
           </span>
 
           <Separator orientation="vertical" className="h-6" />
